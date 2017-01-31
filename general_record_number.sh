ed7f2b3d7d2a7645355bace4739fc04900a34e21
Clive Lau <clivelflau@gmail.com>
2017-01-30 23:44:27 -0800

    made code for general_record_number.sh more efficient

--- general_record_number.sh
+++ general_record_number.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 #This script functions like record_number.sh, but it takes one argument $1 the file name and returns a list of the number of times each individual was sampled.
-#Procedures
+#How it works
 #Take $1(a tab-separated file)
 #Remove the header
 #Replace tab separation with comma
 #Cut out the first column
 #Pipe to uniq
-#Send output to list_of_IDs.txt
-#Make while loop, grep search each ID in list_of_IDs.txt in $1 and output number of lines for that ID
+#assign output to variable $list_of_IDs
+#Make for loop, grep search each ID in $list_of_IDs in $1 and output number of lines for that ID
 
 
-tail -n +2 $1 | tr -s "\t" "," | cut -d "," -f 1 | uniq > list_of_IDs.txt
-while read ID
-	do printf "Individual $ID has been sampled "
-		tail -n +2 $1 | tr -s "\t" "," | grep -c ^$ID,
-		echo "time(s)"
-done < list_of_IDs.txt
+list_of_IDs=$(tail -n +2 $1 | tr -s "\t" "," | cut -d "," -f 1 | uniq)
+for ID in $list_of_IDs
+	do 
+		number=$(tail -n +2 $1 | tr -s "\t" "," | grep -c ^$ID,)
+		echo "Individual $ID has been sampled $number time(s)"
+done
+
 
